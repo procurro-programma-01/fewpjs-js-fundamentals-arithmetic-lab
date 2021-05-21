af8e495c4f946f9a5aca2c799c29dcaf76da959f
abraham saldivar <saldivara1010@gmail.com>
2021-05-21 16:26:57 -0500

    commit tree 6092ddc3ef49fede6d04ae60b6d2d8cc084adc68

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+const num1 = 31 
+const num2 = 2  
+let multiply = num1 * num2 
+
+const random = Math.floor(Math.random() * 10 + 1 ) ;
+console.log(random); 
+
+const num3 = 16
+const num4 = 6
+
+let mod = num3 % num4 
+
+let max = Math.max(1, 2, 3, 20, 5);
+
